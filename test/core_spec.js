887d4c3339e5c5522d970ae830f01dd89e7229e2
Luke <lukeaveil@yahoo.com>
2016-09-08 16:37:16 +0100

    adds getWinner and moving to next pair

--- test/core_spec.js
+++ test/core_spec.js
@@ -41,6 +41,46 @@ describe('application logic', () => {
       }));
     });
 
+    it('puts winner of current vote back to entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 days later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 days later': 2
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 hours')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 hours', 'Trainspotting')
+      }));
+    });
+
+    it('puts both from tied vote back to entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 days later'),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 days later': 3
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 hours')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 hours', 'Trainspotting', '28 days later')
+      }));
+    });
+
   });
 
   describe('vote', () => {
